ac51eec4bfe014cfd220636feb397fe7c2dfa617
Andreas Motl <andreas.motl@elmyra.de>
2015-10-17 15:37:30 +0200

    make mqtt adapter prototype actually work

--- src/kotori.node/kotori/hiveeyes/mqtt_adapter.py
+++ src/kotori.node/kotori/hiveeyes/mqtt_adapter.py
@@ -1,32 +1,36 @@
 # -*- coding: utf-8 -*-
+# (c) 2015 Andreas Motl, Elmyra UG <andreas.motl@elmyra.de>
 # https://pypi.python.org/pypi/twisted-mqtt
 # https://github.com/astrorafael/twisted-mqtt/
-from twisted.logger import Logger, LogLevel
+from twisted.logger import Logger
 from twisted.internet import reactor, task
 from twisted.application.service import Service
 from twisted.internet.endpoints import TCP4ClientEndpoint
-
 from mqtt.client.factory import MQTTFactory
-from mqtt.logger import startLogging
 
 log = Logger()
 
 
-class MyService(Service):
+class HiveeyesMqttAdapter(Service):
 
     def gotProtocol(self, p):
         self.protocol = p
-        d = p.connect("TwistedMQTT-pubsubs", keepalive=0)
+        d = p.connect("kotori.mqtt", keepalive=0)
         d.addCallback(self.subscribe)
-        d.addCallback(self.prepareToPublish)
+        #d.addCallback(self.prepareToPublish)
 
     def subscribe(self, *args):
-        d = self.protocol.subscribe("foo/bar/baz", 0 )
+        d = self.protocol.subscribe("foo/bar/baz", 0)
+        e = self.protocol.subscribe("hiveeyes/#", 0)
         self.protocol.setPublishHandler(self.onPublish)
 
     def onPublish(self, topic, payload, qos, dup, retain, msgId):
-        log.debug("topic = {topic}, msg={payload} qos = {qos}, dup ={dup} retain={retain}, msgId={id}", topic=topic, payload=payload,
+        log.debug("topic={topic}, msg={payload} qos={qos}, dup={dup} retain={retain}, msgId={id}", topic=topic, payload=payload,
             qos=qos, dup=dup, retain=retain, id=msgId)
+
+        if topic.startswith('hiveeyes'):
+            # TODO: store to database
+            pass
 
     def prepareToPublish(self, *args):
         self.task = task.LoopingCall(self.publish)
@@ -41,14 +45,12 @@ class MyService(Service):
         reactor.stop()
 
 
-if __name__ == '__main__':
-    import sys
-    log = Logger()
-    startLogging(sys.stdout, LogLevel.debug)
+def he_boot_mqtt_adapter(broker_host, broker_port=1883, debug=False):
+
+    print 'INFO: Starting MQTT adapter. broker=', broker_host, broker_port
 
     factory = MQTTFactory(profile=MQTTFactory.PUBLISHER | MQTTFactory.SUBSCRIBER)
-    point   = TCP4ClientEndpoint(reactor, "test.mosquitto.org", 1883)
-    serv    = MyService()
+    point   = TCP4ClientEndpoint(reactor, broker_host, broker_port)
+    serv    = HiveeyesMqttAdapter()
 
     d = point.connect(factory).addCallback(serv.gotProtocol)
-    reactor.run()
