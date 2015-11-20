c11ff51175da2f2e69c0a7cb6770fbb7d7134a7f
Andreas Motl <andreas.motl@elmyra.de>
2015-11-20 16:54:13 +0100

    add lst-sattracker application

--- etc/headers/sattracker.h
+++ etc/headers/sattracker.h
@@ -1,10 +1,14 @@
+#include "stdint.h"
+
 typedef struct struct_position         // added 06.03.2014 C.L.
 {
+    /*
     struct_position()
     : length(9), ID(1)
     {}
-    uint8_t  length             ;//1
-    uint8_t  ID                 ;//2
+    */
+    uint8_t  length = 9         ;//1
+    uint8_t  ID     = 1         ;//2
     uint16_t flag_1             ;//4
     int16_t  hdg                ;//6
     int16_t  pitch              ;//8
