94d7a30ab0c5ee1175ac4f176333e8b48b19f37e
Andreas Motl <andreas.motl@elmyra.de>
2016-05-22 19:47:55 +0200

    Bump version: 0.7.0 → 0.7.1

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ extras = {
 }
 
 setup(name='kotori',
-      version='0.7.0',
+      version='0.7.1',
       description='Kotori data acquisition and graphing toolkit',
       long_description='Kotori data acquisition and graphing toolkit',
       license="AGPL 3, EUPL 1.2",
@@ -83,7 +83,7 @@ setup(name='kotori',
       install_requires=requires,
       extras_require=extras,
       dependency_links=[
-          #'https://github.com/tavendo/AutobahnPython/tarball/cb322f78ffaa2a5#egg=autobahn-0.7.0',
+          #'https://github.com/tavendo/AutobahnPython/tarball/cb322f78ffaa2a5#egg=autobahn-0.7.1',
       ],
       entry_points={
           'console_scripts': [
