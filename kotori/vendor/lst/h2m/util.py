b5146035b7f4ae641a53bb956e9afee62c50c347
Andreas Motl <andreas.motl@elmyra.de>
2016-06-17 02:48:04 +0200

    Change cache directory for vendor LST

--- kotori/vendor/lst/h2m/util.py
+++ kotori/vendor/lst/h2m/util.py
@@ -6,7 +6,7 @@ from kotori.daq.intercom.c import LibraryAdapter, StructRegistryByID
 #from kotori.daq.intercom.cffi_adapter import LibraryAdapterCFFI
 
 def setup_h2m_structs_pyclibrary():
-    cache_dir = user_cache_dir('lst', 'elmyra')
+    cache_dir = os.path.join(user_cache_dir('kotori'), 'lst')
     if not os.path.isdir(cache_dir): os.makedirs(cache_dir)
     lib_dir = os.path.join(os.path.dirname(__file__), 'cpp')
     library = LibraryAdapter(u'h2m_structs.h', u'h2m_structs.so', include_path=lib_dir, library_path=lib_dir, cache_path=cache_dir)
@@ -14,7 +14,7 @@ def setup_h2m_structs_pyclibrary():
     return struct_registry
 
 def setup_h2m_structs_cffi():
-    cache_dir = user_cache_dir('lst', 'elmyra')
+    cache_dir = os.path.join(user_cache_dir('kotori'), 'lst')
     if not os.path.isdir(cache_dir): os.makedirs(cache_dir)
     lib_dir = os.path.join(os.path.dirname(__file__), 'cpp')
     library = LibraryAdapterCFFI(u'h2m_structs.h', u'h2m_structs.so', include_path=lib_dir, library_path=lib_dir, cache_path=cache_dir)
