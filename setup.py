592502e38d582268b8f7c4555ce293f814ff0acc
Andreas Motl <andreas.motl@elmyra.de>
2016-07-12 12:29:50 +0200

    Bump version: 0.10.8 → 0.10.9

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ extras = {
 }
 
 setup(name='kotori',
-      version='0.10.8',
+      version='0.10.9',
       description='Kotori data acquisition, routing and graphing toolkit',
       long_description='Kotori data acquisition, routing and graphing toolkit',
       license="AGPL 3, EUPL 1.2",
@@ -154,7 +154,7 @@ setup(name='kotori',
       install_requires=requires,
       extras_require=extras,
       dependency_links=[
-          #'https://github.com/tavendo/AutobahnPython/tarball/cb322f78ffaa2a5#egg=autobahn-0.10.8',
+          #'https://github.com/tavendo/AutobahnPython/tarball/cb322f78ffaa2a5#egg=autobahn-0.10.9',
           'https://github.com/jjmalina/pyinfluxql/tarball/d92db4ab8c#egg=pyinfluxql-0.0.1',
       ],
       entry_points={
