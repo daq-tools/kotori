f87f71babc8db94f0d2ddbc1f5de48554f7f619f
Andreas Motl <andreas.motl@elmyra.de>
2017-03-20 17:51:40 +0100

    Bump version: 0.12.1 → 0.12.2

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ extras = {
 }
 
 setup(name='kotori',
-      version='0.12.1',
+      version='0.12.2',
       description='Kotori data acquisition, routing and graphing toolkit',
       long_description='Kotori data acquisition, routing and graphing toolkit',
       license="AGPL 3, EUPL 1.2",
