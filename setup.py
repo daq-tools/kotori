85909dae4efce52cea9b525c7cd00aa902290414
Andreas Motl <andreas@getkotori.org>
2021-02-10 12:07:42 +0100

    Bump version: 0.25.0 → 0.26.0

--- setup.py
+++ setup.py
@@ -132,7 +132,7 @@ extras = {
 }
 
 setup(name='kotori',
-      version='0.25.0',
+      version='0.26.0',
       description='Kotori is a data acquisition, processing and graphing toolkit for humans',
       long_description=README,
       license="AGPL 3, EUPL 1.2",
