cba244df96232712b031853cb4e6d6313437be0d
Andreas Motl <andreas@getkotori.org>
2021-11-27 21:07:43 +0100

    Add software tests for simulating all advanced actions against Grafana
    - Publish single reading in JSON format to MQTT broker and proof that a
      corresponding datasource and a dashboard was created in Grafana.
    - Publish two subsequent readings in JSON format to MQTT broker and
      proof that a corresponding datasource and a dashboard was first
      created and then updated in Grafana.
    - Publish two subsequent readings to two different topics and proof that
      a corresponding datasource and a dashboard with two panels has been
      created in Grafana.
    - Publish two subsequent readings to two different topics and proof that
      a corresponding datasource and two dashboards have been created in
      Grafana.

--- test/settings/mqttkit.py
+++ test/settings/mqttkit.py
@@ -11,17 +11,20 @@ class TestSettings:
 
     # InfluxDB settings.
     influx_database = 'mqttkit_1_itest'
+    influx_databases = ['mqttkit_1_itest', 'mqttkit_1_itest3']
     influx_measurement_sensors = 'foo_bar_sensors'
     influx_measurement_events = 'foo_bar_events'
 
     # Grafana settings.
     grafana_username = 'admin'
     grafana_password = 'admin'
-    grafana_dashboards = ['mqttkit-1-itest']
+    grafana_dashboards = ['mqttkit-1-itest', 'mqttkit-1-itest3']
 
     # MQTT channel settings.
     mqtt_topic_single = 'mqttkit-1/itest/foo/bar/data'
     mqtt_topic_json   = 'mqttkit-1/itest/foo/bar/data.json'
+    mqtt_topic2_json   = 'mqttkit-1/itest/foo/bar2/data.json'
+    mqtt_topic3_json   = 'mqttkit-1/itest3/foo/bar/data.json'
     mqtt_topic_event  = 'mqttkit-1/itest/foo/bar/event.json'
     mqtt_topic_homie  = 'mqttkit-1/itest/foo/bar/data/__json__'
     mqtt_topic_json_legacy = 'mqttkit-1/itest/foo/bar/message-json'
--- test/test_daq_grafana.py
+++ test/test_daq_grafana.py
@@ -13,10 +13,90 @@ logger = logging.getLogger(__name__)
 
 @pytest_twisted.inlineCallbacks
 @pytest.mark.grafana
-def test_mqtt_to_grafana(machinery, create_influxdb, reset_influxdb, reset_grafana):
+def test_mqtt_to_grafana_single(machinery, create_influxdb, reset_influxdb, reset_grafana):
     """
     Publish single reading in JSON format to MQTT broker and proof
     that a corresponding datasource and a dashboard was created in Grafana.
+    """
+
+    # Submit a single measurement, without timestamp.
+    data = {
+        'temperature': 42.84,
+        'humidity': 83.1,
+    }
+    yield mqtt_json_sensor(settings.mqtt_topic_json, data)
+
+    # Wait for some time to process the message.
+    yield sleep(PROCESS_DELAY_MQTT)
+
+    # Proof that Grafana is well provisioned.
+    logger.info('Grafana: Checking datasource')
+    assert settings.influx_database in grafana.get_datasource_names()
+
+    logger.info('Grafana: Retrieving dashboard')
+    dashboard_name = settings.grafana_dashboards[0]
+    dashboard = grafana.get_dashboard_by_name(dashboard_name)
+
+    logger.info('Grafana: Checking dashboard layout')
+    target = dashboard['dashboard']['rows'][0]['panels'][0]['targets'][0]
+    assert target['measurement'] == settings.influx_measurement_sensors
+    assert 'temperature' in target['query'] or 'humidity' in target['query']
+
+
+@pytest_twisted.inlineCallbacks
+@pytest.mark.grafana
+def test_mqtt_to_grafana_update_panel(machinery, create_influxdb, reset_influxdb, reset_grafana):
+    """
+    Publish two subsequent readings in JSON format to MQTT broker and proof
+    that a corresponding datasource and a dashboard was first created and
+    then updated in Grafana.
+    """
+
+    # Submit a single measurement, without timestamp.
+    data = {
+        'temperature': 42.84,
+        'humidity': 83.1,
+    }
+    yield mqtt_json_sensor(settings.mqtt_topic_json, data)
+
+    # Wait for some time to process the message.
+    yield sleep(PROCESS_DELAY_MQTT)
+
+    # Proof that Grafana is well provisioned.
+    logger.info('Grafana: Checking datasource')
+    assert settings.influx_database in grafana.get_datasource_names()
+
+    logger.info('Grafana: Retrieving dashboard')
+    dashboard_name = settings.grafana_dashboards[0]
+    dashboard = grafana.get_dashboard_by_name(dashboard_name)
+
+    logger.info('Grafana: Checking measurement of first target')
+    target = dashboard['dashboard']['rows'][0]['panels'][0]['targets'][0]
+    assert target['measurement'] == settings.influx_measurement_sensors
+
+    logger.info('Grafana: Checking panel fields after creation')
+    assert grafana.get_field_names(dashboard_name=dashboard_name, panel_index=0) == ["humidity", "temperature"]
+
+    # Submit another measurement, without timestamp.
+    data = {
+        'pressure': 1024,
+    }
+    yield mqtt_json_sensor(settings.mqtt_topic_json, data)
+
+    # Wait for some time to process the message.
+    yield sleep(PROCESS_DELAY_MQTT)
+
+    logger.info('Grafana: Checking panel fields after update')
+    assert grafana.get_field_names(dashboard_name=dashboard_name, panel_index=0) == ["humidity", "pressure", "temperature"]
+
+
+@pytest_twisted.inlineCallbacks
+@pytest.mark.grafana
+def test_mqtt_to_grafana_two_panels(machinery, create_influxdb, reset_influxdb, reset_grafana):
+    """
+    Publish two subsequent readings to two different topics and proof that a
+    corresponding datasource and a dashboard with two panels has been created
+    in Grafana.
     """
 
     # Submit a single measurement, without timestamp.
@@ -33,16 +113,61 @@ def test_mqtt_to_grafana(machinery, create_influxdb, reset_influxdb, reset_grafa
 
     # Proof that Grafana is well provisioned.
     logger.info('Grafana: Checking datasource')
-    datasource_names = []
-    for datasource in grafana.client.datasources.get():
-        datasource_names.append(datasource['name'])
-    assert settings.influx_database in datasource_names
+    assert settings.influx_database in grafana.get_datasource_names()
 
-    logger.info('Grafana: Retrieving dashboard')
     dashboard_name = settings.grafana_dashboards[0]
-    dashboard = grafana.get_dashboard_by_name(dashboard_name)
 
-    logger.info('Grafana: Checking dashboard layout')
-    target = dashboard['dashboard']['rows'][0]['panels'][0]['targets'][0]
-    assert target['measurement'] == settings.influx_measurement_sensors
-    assert 'temperature' in target['query'] or 'humidity' in target['query']
+    logger.info('Grafana: Check number of panels after creation')
+    assert len(grafana.get_panels(dashboard_name)) == 1
+
+    # Submit measurement to another topic.
+    yield mqtt_json_sensor(settings.mqtt_topic2_json, data)
+
+    # Wait for some time to process the message.
+    yield sleep(PROCESS_DELAY_MQTT)
+
+    logger.info('Grafana: Check number of panels after creation')
+    assert len(grafana.get_panels(dashboard_name)) == 2
+
+
+@pytest_twisted.inlineCallbacks
+@pytest.mark.grafana
+def test_mqtt_to_grafana_two_dashboards(machinery, create_influxdb, reset_influxdb, reset_grafana):
+    """
+    Publish two subsequent readings to two different topics and proof that a
+    corresponding datasource and two dashboards have been created in Grafana.
+    """
+
+    # Submit a single measurement, without timestamp.
+    data = {
+        'temperature': 42.84,
+        'humidity': 83.1,
+    }
+    yield mqtt_json_sensor(settings.mqtt_topic_json, data)
+
+    # Wait for some time to process the message.
+    yield sleep(PROCESS_DELAY_MQTT)
+    yield sleep(PROCESS_DELAY_MQTT)
+    yield sleep(PROCESS_DELAY_MQTT)
+
+    # Proof that Grafana is well provisioned.
+    logger.info('Grafana: Checking datasource')
+    assert settings.influx_database in grafana.get_datasource_names()
+
+    logger.info('Grafana: Check dashboards after creation')
+    titles = grafana.get_dashboard_titles()
+    assert settings.grafana_dashboards[0] in titles
+    assert settings.grafana_dashboards[1] not in titles
+
+    # Submit measurement to another topic.
+    yield mqtt_json_sensor(settings.mqtt_topic3_json, data)
+
+    # Wait for some time to process the message.
+    yield sleep(PROCESS_DELAY_MQTT)
+    yield sleep(PROCESS_DELAY_MQTT)
+    yield sleep(PROCESS_DELAY_MQTT)
+
+    logger.info('Grafana: Check dashboards after update')
+    titles = grafana.get_dashboard_titles()
+    assert settings.grafana_dashboards[0] in titles
+    assert settings.grafana_dashboards[1] in titles
