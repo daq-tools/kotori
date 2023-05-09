dc1e6fc712b83721bdc6f79ba65762807d856361
Matthias Büchner <m.buechner@gmail.com>
2023-05-09 10:13:46 +0200

    [TTN] Decode metadata from full TTN payload

--- kotori/daq/decoder/tts_ttn.py
+++ kotori/daq/decoder/tts_ttn.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
-# (c) 2022 Andreas Motl <andreas@getkotori.org>
+# (c) 2019-2023 Andreas Motl <andreas@getkotori.org>
+# (c) 2019-2023 Matthias Büchner <m.buechner@gmail.com>
 import json
 from collections import OrderedDict
+import typing as t
+
+
+MessageValueType = t.Dict[str, t.Union[str, int, float, t.Dict[str, t.Any], t.List[t.Dict]]]
 
 
 class TheThingsStackDecoder:
@@ -17,25 +22,74 @@ class TheThingsStackDecoder:
     ==========
     - https://www.thethingsindustries.com/docs/the-things-stack/concepts/data-formats/#uplink-messages
     - https://www.thethingsindustries.com/docs/integrations/webhooks/
+    - https://www.thethingsnetwork.org/docs/lorawan/architecture/
+    - https://www.thethingsnetwork.org/docs/lorawan/message-types/
     - https://community.hiveeyes.org/t/more-data-acquisition-payload-formats-for-kotori/1421
     - https://community.hiveeyes.org/t/tts-ttn-daten-an-kotori-weiterleiten/1422/34
     """
 
-    @staticmethod
-    def decode(payload: str):
+    @classmethod
+    def decode(cls, payload: str):
 
         # Decode from JSON.
         message = json.loads(payload)
 
-        # Use timestamp and decoded payload.
         data = OrderedDict()
+
+        # Decode device id, timestamp, and decoded uplink message payload.
+        if "end_device_ids" in message:
+            data["device_id"] = message["end_device_ids"]["device_id"]
         if "received_at" in message:
             data["timestamp"] = message["received_at"]
-        data.update(message["uplink_message"]["decoded_payload"])
 
-        # TODO: Add more data / metadata.
-        #       This is an implementation from scratch. It can be improved by
-        #       cherry-picking more specific decoding routines from `ttnlogger`.
-        #       https://github.com/mqtt-tools/ttnlogger
+        if "uplink_message" in message:
+            data.update(cls.decode_uplink_message(message["uplink_message"]))
 
         return data
+
+    @classmethod
+    def decode_uplink_message(cls, uplink_message: MessageValueType):
+        """
+        Decode a TTN uplink message, i.e. originating from the appliance/device.
+        """
+
+        data = OrderedDict()
+
+        # Decode message payload.
+        data.update(uplink_message["decoded_payload"])
+
+        # Extract infrastructure data.
+        if "settings" in uplink_message:
+            data["bw"] = uplink_message["settings"]["data_rate"]["lora"]["bandwidth"] / 1000
+            data["sf"] = uplink_message["settings"]["data_rate"]["lora"]["spreading_factor"]
+            data["freq"] = float(uplink_message["settings"]["frequency"]) / 1000000.0
+        if "f_cnt" in uplink_message:
+            data["counter"] = int(uplink_message["f_cnt"])
+        if "rx_metadata" in uplink_message:
+            data["gtw_count"] = len(uplink_message["rx_metadata"])
+            for rx_metadata in uplink_message["rx_metadata"]:
+                gateway_id = rx_metadata["gateway_ids"]["gateway_id"]
+                data["gw_" + gateway_id + "_rssi"] = rx_metadata["rssi"]
+                data["gw_" + gateway_id + "_snr"] = rx_metadata["snr"]
+
+        return data
+
+
+if __name__ == "__main__":
+    """
+    About
+    =====
+    Decode a TTN JSON payload file on the command line.
+
+    Synopsis
+    ========
+    ::
+
+        python -m kotori.daq.decoder.tts_ttn "test/test_tts_ttn_full.json"
+        python -m kotori.daq.decoder.tts_ttn "test/test_tts_ttn_minimal.json"
+    """
+    import sys
+
+    filepath = sys.argv[1]
+    data = TheThingsStackDecoder.decode(open(filepath).read())
+    print(json.dumps(data, indent=2))
